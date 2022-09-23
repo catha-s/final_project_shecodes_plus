a1edd861bf716ce1627a1fff710e6978adddeb66
catha-s <catha-s@users.noreply.github.com>
2022-09-23 15:44:02 +0200

    Added weather details

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temp");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#windspeed");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windspeedElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "9cb72bec958f8fb02391985ed7b219d2";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiURL).then(displayTemperature);
